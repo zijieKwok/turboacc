4c6943788d68f7cd5d2c50f48ac4a1de7f890d2e
zijieKwok <109743406+zijieKwok@users.noreply.github.com>
2024-01-13 01:42:34 +0800

    Update add_turboacc.sh

--- add_turboacc.sh
+++ add_turboacc.sh
@@ -26,9 +26,9 @@ if [ -d "./package/turboacc" ]; then
     fi
 fi
 
-git clone --depth=1 --single-branch --branch "package" https://github.com/chenmozhijin/turboacc "$TMPDIR/package" || exit 1
+git clone --depth=1 --single-branch --branch "package" https://github.com/zijieKwok/turboacc "$TMPDIR/package" || exit 1
 git clone --depth=1 --single-branch https://github.com/fullcone-nat-nftables/nft-fullcone "$TMPDIR/turboacc/nft-fullcone" || exit 1
-git clone --depth=1 --single-branch https://github.com/chenmozhijin/turboacc "$TMPDIR/turboacc/turboacc" || exit 1
+git clone --depth=1 --single-branch https://github.com/zijieKwok/turboacc "$TMPDIR/turboacc/turboacc" || exit 1
 cp -r "$TMPDIR/turboacc/turboacc/luci-app-turboacc" "$TMPDIR/turboacc/luci-app-turboacc"
 rm -rf "$TMPDIR/turboacc/turboacc"
 cp -r "$TMPDIR/package/shortcut-fe" "$TMPDIR/turboacc/shortcut-fe"
